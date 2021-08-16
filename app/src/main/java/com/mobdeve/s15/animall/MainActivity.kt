6454f1a02ad3053ffe8abd82d290542ad28b501a
neil matthew lua <neil_lua@dlsu.edu.ph>
2021-08-16 10:26:52 +0800

    Initial commit

---
+++ app/src/main/java/com/mobdeve/s15/animall/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mobdeve.s15.animall
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
