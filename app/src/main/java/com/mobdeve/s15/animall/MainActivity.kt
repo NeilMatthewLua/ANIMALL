3e122fecb93635a5c74f6b6e0219eaa18366c0b3
neil matthew lua <neil_lua@dlsu.edu.ph>
2021-08-20 14:52:57 +0800

    Single upload setup complete

--- app/src/main/java/com/mobdeve/s15/animall/MainActivity.kt
+++ app/src/main/java/com/mobdeve/s15/animall/MainActivity.kt
@@ -1,11 +1,182 @@
 package com.mobdeve.s15.animall
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.ProgressDialog
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.ktx.storage
+import java.io.IOException
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    // views for button
+    private lateinit var btnSelect: Button
+    private lateinit var btnUpload: Button
+
+    // view for image view
+    private lateinit var imageView: ImageView
+
+    // Uri indicates, where the image will be picked from
+    private lateinit var filePath: Uri
+
+    // request code
+    private var PICK_IMAGE_REQUEST = 22
+
+    // instance for firebase storage and StorageReference
+    lateinit var storage: FirebaseStorage
+    lateinit var storageReference: StorageReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val actionBar: ActionBar?
+        actionBar = supportActionBar
+        val colorDrawable = ColorDrawable(
+            Color.parseColor("#0F9D58")
+        )
+        actionBar!!.setBackgroundDrawable(colorDrawable)
+
+        // initialise views
+
+        // initialise views
+        btnSelect = findViewById(R.id.btnChoose)
+        btnUpload = findViewById(R.id.btnUpload)
+        imageView = findViewById(R.id.imgView)
+
+        // get the Firebase  storage reference
+
+        // get the Firebase  storage reference
+        storage = Firebase.storage
+        storageReference = storage.reference
+
+        // on pressing btnSelect SelectImage() is called
+        btnSelect.setOnClickListener{
+            SelectImage();
+        };
+
+        // on pressing btnUpload uploadImage() is called
+        btnUpload.setOnClickListener{
+            uploadImage();
+        };
+    }
+
+    // Select Image method
+    private fun SelectImage() {
+        // Defining Implicit Intent to mobile gallery
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(
+            Intent.createChooser(
+                intent,
+                "Select Image from here..."
+            ),
+            PICK_IMAGE_REQUEST
+        )
+    }
+
+    // Override onActivityResult method
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int,
+        data: Intent?
+    ) {
+        super.onActivityResult(
+            requestCode,
+            resultCode,
+            data
+        )
+
+        // checking request code and result code
+        // if request code is PICK_IMAGE_REQUEST and
+        // resultCode is RESULT_OK
+        // then set image in the image view
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.data != null) {
+
+            // Get the Uri of data
+            filePath = data.data!!
+            try {
+
+                // Setting image on image view using Bitmap
+                val bitmap = MediaStore.Images.Media
+                    .getBitmap(
+                        contentResolver,
+                        filePath
+                    )
+                imageView.setImageBitmap(bitmap)
+            } catch (e: IOException) {
+                // Log the exception
+                e.printStackTrace()
+            }
+        }
+    }
+
+    // UploadImage method
+    private fun uploadImage() {
+        if (filePath != null) {
+
+            // Code for showing progressDialog while uploading
+            val progressDialog = ProgressDialog(this)
+            progressDialog.setTitle("Uploading...")
+            progressDialog.show()
+
+            // Defining the child of storageReference
+            val ref: StorageReference = storageReference
+                .child(
+                    "images/69696969/"
+                            + UUID.randomUUID().toString()
+                )
+
+            // adding listeners on upload
+            // or failure of image
+            ref.putFile(filePath)
+                .addOnSuccessListener { // Image uploaded successfully
+                    // Dismiss dialog
+                    progressDialog.dismiss()
+                    Toast
+                        .makeText(
+                            this@MainActivity,
+                            "Image Uploaded!!",
+                            Toast.LENGTH_SHORT
+                        )
+                        .show()
+                }
+                .addOnFailureListener { e -> // Error, Image not uploaded
+                    progressDialog.dismiss()
+                    Toast
+                        .makeText(
+                            this@MainActivity,
+                            "Failed " + e.message,
+                            Toast.LENGTH_SHORT
+                        )
+                        .show()
+                }
+                .addOnProgressListener { taskSnapshot ->
+
+                    // Progress Listener for loading
+                    // percentage on the dialog box
+                    val progress = (100.0
+                            * taskSnapshot.bytesTransferred
+                            / taskSnapshot.totalByteCount)
+                    progressDialog.setMessage(
+                        "Uploaded "
+                                + progress.toInt() + "%"
+                    )
+                }
+        }
     }
 }
