5b6476c8505f011494a137ff687a6f4a68ff9c75
Kurotwo <kurt_bradley_tanting@dlsu.edu.ph>
2021-08-21 11:10:06 +0800

    Initial navbar using fragments

--- app/src/main/java/com/mobdeve/s15/animall/MainActivity.kt
+++ app/src/main/java/com/mobdeve/s15/animall/MainActivity.kt
@@ -2,10 +2,23 @@ package com.mobdeve.s15.animall
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Initialize the bottom navigation view
+        //create bottom navigation view object
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        findViewById<BottomNavigationView>(R.id.bottom_navigatin_view)
+            .setupWithNavController(navController)
+
     }
 }
