84b894c01c39202c35024ce39e08c42144c88df2
neil matthew lua <neil_lua@dlsu.edu.ph>
2021-08-24 12:59:49 +0800

    Started UI of profile page

--- app/src/main/java/com/mobdeve/s15/animall/MainActivity.kt
+++ app/src/main/java/com/mobdeve/s15/animall/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.mobdeve.s15.animall
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewProfileBtn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewProfileBtn = findViewById(R.id.viewProfileBtn)
+
+        viewProfileBtn.setOnClickListener{
+//            val manager: FragmentManager = getSupportFragmentManager()
+//            val transaction: FragmentTransaction = manager.beginTransaction()
+//            transaction.add(R.id.container, UserProfileFragment, "UserProfileFragment")
+//            transaction.addToBackStack(null)
+//            transaction.commit()
+        }
     }
 }
