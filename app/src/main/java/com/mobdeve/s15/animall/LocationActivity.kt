8367d9b85f778246ca52b12ae1a1642789f09c3e
Carlos Shi <carlos_shi@dlsu.edu.ph>
2021-08-24 14:05:45 +0800

    Added UI for location activity

--- app/src/main/java/com/mobdeve/s15/animall/LocationActivity.kt
+++ app/src/main/java/com/mobdeve/s15/animall/LocationActivity.kt
@@ -2,17 +2,26 @@ package com.mobdeve.s15.animall
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 class LocationActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var bottomSheet: ConstraintLayout
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // Set the layout file as the content view.
         setContentView(R.layout.activity_location)
+
+        bottomSheet = findViewById(R.id.bottom_sheet_layout)
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
 
         // Get a handle to the fragment and register the callback.
         val mapFragment = supportFragmentManager
