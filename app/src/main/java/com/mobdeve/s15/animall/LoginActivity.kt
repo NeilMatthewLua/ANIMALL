7783a654870a60ddadb0c7b687335fee9d346db4
Carlos Shi <carlos_shi@dlsu.edu.ph>
2021-09-16 22:39:47 +0800

    optimize import

--- app/src/main/java/com/mobdeve/s15/animall/LoginActivity.kt
+++ app/src/main/java/com/mobdeve/s15/animall/LoginActivity.kt
@@ -7,7 +7,6 @@ import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import androidx.activity.result.ActivityResult
-import androidx.activity.result.ActivityResultCallback
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
